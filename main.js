7260cc568f38c6902d1ee69c65eb5bff49a4e8b7
Luke Cloninger <Luke.Cloninger@clevelandcliffs.com>
2022-07-21 21:01:11 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { sign } from "sign";
+import { program } from "commander";
+
+program
+  .option("-t, --type <type>", "type of signature")
+  .option("-u, --url [url...]", "URLs which can use the plugin")
+
+program.parse();
+
+const [type, url] = program.opts();
+sign(type, url);
